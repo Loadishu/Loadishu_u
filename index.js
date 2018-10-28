b4295f978965fb2da017a5190a00efc00bd38bab
Loadishu <44552790+Loadishu@users.noreply.github.com>
2018-10-28 15:07:07 -0400

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 const bot = new Discord.Client();
 const cfg require('./index.json'); 
 const token = process.env.token 
-const prefix = ("cfg.prefix");
+const prefix = ("?");
 bot.on('ready', function () {
     console.log("Je suis prêt à être utilisé.")
     bot.user.setActivity('rien').catch(console.error)
